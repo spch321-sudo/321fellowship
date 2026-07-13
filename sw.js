56c230ef4241337468a79a02e307d5681c608595
spch321-sudo <spch321@gmail.com>
2026-07-13 17:59:18 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@ var SHELL = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", function(e){
